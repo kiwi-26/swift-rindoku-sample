bf2acf51c51a5f77d77c33a4f85b454a9dca2864
kiwi-26 <winter-wind.sh@hotmail.co.jp>
2018-09-27 11:38:43 +0900

    ListViewControllerをTableViewDelegate, TableViewDataSourceに準拠

--- swift-rindoku-sample/ListViewController.swift
+++ swift-rindoku-sample/ListViewController.swift
@@ -15,8 +15,24 @@ class ListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ListViewController: UITableViewDelegate {
+    
+}
+
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = "Hoge"
+        return cell
+    }
+}
