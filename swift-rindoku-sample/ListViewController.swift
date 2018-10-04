62f91062a0604f95c50408be1614dd9d5d61b7de
kiwi-26 <winter-wind.sh@hotmail.co.jp>
2018-10-04 11:21:52 +0900

    データを配列から取得するように変更

--- swift-rindoku-sample/ListViewController.swift
+++ swift-rindoku-sample/ListViewController.swift
@@ -12,6 +12,8 @@ class ListViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView!
     let cellId = "cellId"
+    
+    let data = ["hoge", "fuga", "piyo"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,12 +35,12 @@ extension ListViewController: UITableViewDelegate {
 
 extension ListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        cell.textLabel?.text = "Hoge"
+        cell.textLabel?.text = data[indexPath.row]
         return cell
     }
 }
