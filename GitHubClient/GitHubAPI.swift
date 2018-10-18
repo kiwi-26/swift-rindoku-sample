e27aa2779f6d13208b6db4ffe10193867b2bfa1c
kiwi-26 <winter-wind.sh@hotmail.co.jp>
2018-10-18 11:33:14 +0900

    リポジトリ検索APIを追加

---
+++ GitHubClient/GitHubAPI.swift
@@ -0,0 +1,30 @@
+//
+//  GitHubAPI.swift
+//  GitHubClient
+//
+//  Created by SCI01557 on 2018/10/18.
+//  Copyright © 2018年 hicka04. All rights reserved.
+//
+
+import Foundation
+
+final class GitHubAPI {
+    struct SearchRepositories: GitHubRequest {        
+        let keyword: String
+        var body: Encodable?
+
+        typealias Response = SearchResponse<Repository>
+        
+        var method: HTTPMethod {
+            return .get
+        }
+        
+        var path: String {
+            return "/search/repositories"
+        }
+        
+        var queryItems: [URLQueryItem] {
+            return [URLQueryItem(name: "q", value: keyword)]
+        }
+    }
+}
